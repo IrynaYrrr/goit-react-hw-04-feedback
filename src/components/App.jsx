ab6cab44a6db0326fdb46ce2d70d6cf613e12736
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2023-10-02 13:26:36 +0200

    change HW from class to functional components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,64 +1,59 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  };
 
-  onLeaveFeedback = (option) => {
-    this.setState((prevState) => {
-      return {
-        [option]: prevState[option] + 1,
-      }
-    })
+export const App = () => {
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   }
 
-  countTotalFeedback() {
-    return this.state.good + this.state.neutral + this.state.bad;
-  }
-
-  countPositiveFeedbackPercentage() {
-    const persentage = Math.round(this.state.good / this.countTotalFeedback() * 100)
+  const countPositiveFeedbackPercentage = () => {
+    const persentage = Math.round(good / countTotalFeedback() * 100)
     return isNaN(persentage) ? 0 : persentage
   }
 
-  render() {
+  return (
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: 40,
+      color: '#010101'
+    }}>
 
-    return (
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}>
-
-        <Section title='Please leave feedback'>
-        <FeedbackOptions options={Object.keys(this.state)} onLeaveFeedback={this.onLeaveFeedback} />
+      <Section title='Please leave feedback'>
+        <FeedbackOptions
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          setGood={setGood}
+          setNeutral={setNeutral}
+          setBad={setBad}
+        />
+      </Section>
+      {countTotalFeedback() !== 0
+        ?
+        <Section title='Statistics'>
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
         </Section>
-        {this.countTotalFeedback() !== 0
-          ?
-          <Section title='Statistics'>
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          </Section>
-          :
-          <Notification title='There is no feedback' />
-        }
-
-      </div>
-    );
-  }
+        :
+        <Notification title='There is no feedback' />
+      }
+    </div>
+  )
 }
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import css from './FeedbackOptions.module.css';
 import PropTypes from 'prop-types';
 
-export const FeedbackOptions = ({options, onLeaveFeedback}) => {
+export const FeedbackOptions = ({ good, neutral, bad, setGood, setNeutral, setBad }) => {
   return (
     <div className={css.feedbackButtons}>
-      {options.map((button) => (
-          <button className={css[button]} onClick={() => onLeaveFeedback(button)} type="button">
-            {button}
-          </button>
-      ))}
+      <button className={css['good']} onClick={() => setGood(good + 1)} type="button">
+        good
+      </button>
+      <button className={css['neutral']} onClick={() => setNeutral(neutral + 1)} type="button">
+        neutral
+      </button>
+      <button className={css['bad']} onClick={() => setBad(bad + 1)} type="button">
+        bad
+      </button>
     </div>
   )
 }
